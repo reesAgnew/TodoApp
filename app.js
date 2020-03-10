62298d347536142e925754371487ae0652ce0997
Rees Agnew <rees.agnew1@gmail.com>
2020-03-10 17:44:43 -0400

    Cont. Setting Up App

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bodyParser = require("body-parser");
 var request = require("request");
 var methodOverride = require ("method-override");
 var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/blog_app', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/todo_app', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
 
 
@@ -22,44 +22,58 @@ app.use(methodOverride("_method"));
 
 //=========SCHEMA SETUP=============================
 var todoSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
+    todo: String,
+    details: String
 });
 
 var Todo = mongoose.model("Todo", todoSchema);
 
-Todo.create(
-    {
-        name: "Granite Hill",
-        image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
-        description: "This is a huge granite hill, no bathrooms, no water, beautiful granite."
-    },
-    function (err, campground) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("NEWLY CREATED CAMPGROUND: ");
-            console.log(campground);
-        }
-    }
-);
+//HARD CODE TODO TO CHECK DB 
+// Todo.create(
+//     {
+//         todo: "Walk Dogs",
+//         details: "Take dogs for walk, a couple of blocks minimum."
+//     },
+//     function (err, todo) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("NEWLY CREATED TODO: ");
+//             console.log(todo);
+//         }
+//     }
+// );
 //=========HOMEPAGE=================================
-//replave "/blogs" with whatever the index route is
 app.get("/", function (req, res) {
     res.redirect("/todos")
 });
 //=========ROUTES===================================
 
+//INDEX ROUTE
 
 
 
+//NEW ROUTE
 
 
 
+//CREATE ROUTE
 
 
 
+//SHOW ROUTE
+
+
+
+//EDIT ROUTE
+
+
+
+//UPDATE ROUTE
+
+
+
+//DESTROY ROUTE
 
 
 
